d94062db9622083b5292fbde2c02244c72655d44
Hai Dinh Nguyen <dinhhai281@gmail.com>
2021-10-16 17:31:16 +0700

    chore: add eslint, prettier, switch from karma to jest

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+const { pathsToModuleNameMapper } = require('ts-jest/utils');
+const { compilerOptions } = require('./tsconfig');
+
+module.exports = {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: [
+    '<rootDir>/src/setup-jest.ts',
+  ],
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/dist/',
+  ],
+  transformIgnorePatterns: ['^.+\\.js$'],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths ?? {}, { prefix: '<rootDir>/' }),
+}
