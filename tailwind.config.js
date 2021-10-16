5a24be778d42a2065034ca32ba77d0f81e828c00
Hai Dinh Nguyen <dinhhai281@gmail.com>
2021-10-16 17:39:02 +0700

    feat: add tailwindcss for styling

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  mode: 'jit',
+  purge: [
+    'src/**/*.ts'
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
